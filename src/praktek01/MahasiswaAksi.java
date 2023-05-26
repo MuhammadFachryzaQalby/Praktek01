03379eaf7e050c0695e23fdb046b4ba405162390
qalbyreza@gmail.com <qalbyreza@gmail.com>
2023-05-26 09:26:15 +0800

    Mengisi Nilai Atribut

--- src/praktek01/MahasiswaAksi.java
+++ src/praktek01/MahasiswaAksi.java
@@ -3,6 +3,8 @@ package praktek01;
 public class MahasiswaAksi {
     public static void main(String[] args) {
         Mahasiswa Fahri = new Mahasiswa();
-
+        Fahri.nama="Muhammad Fahcryza Qalby";
+        Fahri.NPM="2110010104";
+        Fahri.IPK=3.5;
     }
 }
